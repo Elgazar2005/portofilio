0c63368c70ecb87533a36761805b8ef26bfbe4bb
Elgazar2005 <omarelgazar715@gmail.com>
2026-05-13 22:27:00 +0300

    second

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -56,58 +56,60 @@ function About() {
           </div>
         </div>
       </div>
-      <div className="grid md:grid-cols-3 gap-8 ">
-        <Card className="mt-6 w-96 rounded-lg hover:shadow-lg dark:hover:shadow-accent/10 transition-shadow">
-          <CardBody>
-            <Typography variant="h5" color="blue-gray" className="mb-2 flex gap-3">
-                <div className="bg-light-blue-400/10 dark:bg-primary/20 p-2 rounded-lg">
-                    <Code className="w-5 h-5 text-light-blue-300" />
-                </div>
-              
-              <h3 className="font-['Poppins'] font-semibold text-lg text-foreground">
-                Technical Skills
-              </h3>
-            </Typography>
-            <Typography>
-              Because it&apos;s about motivating the doers. Because I&apos;m
-              here to follow my dreams and inspire others.
-            </Typography>
-          </CardBody>
-        </Card>
-         <Card className="mt-6 w-96 rounded-lg hover:shadow-lg dark:hover:shadow-accent/10 transition-shadow">
-          <CardBody>
-            <Typography variant="h5" color="blue-gray" className="mb-2 flex gap-3">
-               <div className="bg-light-chart-5/40 dark:bg-accent/20 p-2 rounded-lg">
-                <Users className="w-5 h-5 text-accent" />
-              </div>
-              
-              <h3 className="font-['Poppins'] font-semibold text-lg text-foreground">
-                Soft Skills
-              </h3>
-            </Typography>
-            <Typography>
-              Because it&apos;s about motivating the doers. Because I&apos;m
-              here to follow my dreams and inspire others.
-            </Typography>
-          </CardBody>
-        </Card>
-         <Card className="mt-6 w-96 rounded-lg hover:shadow-lg dark:hover:shadow-accent/10 transition-shadow">
-          <CardBody>
-            <Typography variant="h5" color="blue-gray" className="mb-2 flex gap-3">
-                <div className="bg-secondary/10 dark:bg-secondary/20 p-2 rounded-lg">
-                <Globe className="w-5 h-5 text-secondary" />
-              </div>
-              <h3 className="font-['Poppins'] font-semibold text-lg text-foreground">
-                Languages
-              </h3>
-            </Typography>
-            <Typography>
-              Because it&apos;s about motivating the doers. Because I&apos;m
-              here to follow my dreams and inspire others.
-            </Typography>
-          </CardBody>
-        </Card>
-      </div>
+<div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+  
+  <Card className="w-full rounded-lg hover:shadow-lg dark:hover:shadow-accent/10 transition-shadow">
+    <CardBody>
+      <Typography variant="h5" color="blue-gray" className="mb-2 flex gap-3 items-center">
+        <div className="bg-light-blue-400/10 dark:bg-primary/20 p-2 rounded-lg">
+          <Code className="w-5 h-5 text-light-blue-300" />
+        </div>
+        <h3 className="font-['Poppins'] font-semibold text-lg text-foreground">
+          Technical Skills
+        </h3>
+      </Typography>
+      <Typography>
+        Because it&apos;s about motivating the doers. Because I&apos;m
+        here to follow my dreams and inspire others.
+      </Typography>
+    </CardBody>
+  </Card>
+
+  <Card className="w-full rounded-lg hover:shadow-lg dark:hover:shadow-accent/10 transition-shadow">
+    <CardBody>
+      <Typography variant="h5" color="blue-gray" className="mb-2 flex gap-3 items-center">
+        <div className="bg-light-chart-5/40 dark:bg-accent/20 p-2 rounded-lg">
+          <Users className="w-5 h-5 text-accent" />
+        </div>
+        <h3 className="font-['Poppins'] font-semibold text-lg text-foreground">
+          Soft Skills
+        </h3>
+      </Typography>
+      <Typography>
+        Because it&apos;s about motivating the doers. Because I&apos;m
+        here to follow my dreams and inspire others.
+      </Typography>
+    </CardBody>
+  </Card>
+
+  <Card className="w-full rounded-lg hover:shadow-lg dark:hover:shadow-accent/10 transition-shadow">
+    <CardBody>
+      <Typography variant="h5" color="blue-gray" className="mb-2 flex gap-3 items-center">
+        <div className="bg-secondary/10 dark:bg-secondary/20 p-2 rounded-lg">
+          <Globe className="w-5 h-5 text-secondary" />
+        </div>
+        <h3 className="font-['Poppins'] font-semibold text-lg text-foreground">
+          Languages
+        </h3>
+      </Typography>
+      <Typography>
+        Because it&apos;s about motivating the doers. Because I&apos;m
+        here to follow my dreams and inspire others.
+      </Typography>
+    </CardBody>
+  </Card>
+
+</div>
     </section>
   );
 }
